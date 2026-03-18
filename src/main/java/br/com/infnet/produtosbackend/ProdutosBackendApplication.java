bbdc9253e5af0cdebd3b12e69b4728da48d75797
leonardogloria <contato@leonardogloria.com.br>
2026-03-18 17:54:38 -0300

    Initial commit

---
+++ src/main/java/br/com/infnet/produtosbackend/ProdutosBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.infnet.produtosbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdutosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdutosBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/infnet/produtosbackend/ProdutosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.infnet.produtosbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
